2db2df15c677b7def175dbb74d9dcdac6f5bf26b
Michael Kreil <github@michael-kreil.de>
2020-12-31 12:55:26 +0100

    wait for 3 seconds

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -2,6 +2,7 @@
 
 set -e
 cd "$(dirname "$0")"
+set -x
 
 git pull
 
@@ -9,6 +10,8 @@ node 1_download.js
 node 2_deduplicate.js
 node 3_parse.js
 node 4_generate_csv.js
+
+sleep 3
 
 git add ../data/
 git commit -m "automatic data update"
