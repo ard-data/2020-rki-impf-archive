1114a04a5480b4370fe89989a7c417c4599c2be3
Michael Kreil <github@michael-kreil.de>
2021-01-05 18:31:10 +0100

    fix cronjob

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
+source ~/.profile
+
+set -e
+set -x
 cd "$(dirname "$0")"
 
-do_work() {
-	set -x
+git pull
 
-	git pull || return 1
-
-	node 1_download.js || return 1
-	node 2_deduplicate.js || return 1
-	node 3_parse.js || return 1
-	node 4_generate_csv.js || return 1
-
-	sleep 3
-
-	return 0
-}
-
-report() {
-	cat <(echo -e "Subject: Fehler im RKI Impf cronjob\nFrom: root medium hetzner <root@localhost.localdomain>\n\n") cronjob.log <(echo -e "\n\n.") | sendmail -v bots@michael-kreil.de
-}
-
-exec 2>cronjob.log
-
-do_work || report
-
-rm -f cronjob.log
+node 1_download.js
+node 2_deduplicate.js
+node 3_parse.js || true
+node 4_generate_csv.js
 
 git add ../data/
 git commit -m "automatic data update"
