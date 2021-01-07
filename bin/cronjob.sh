1c57724c9e7b8fa8949f74bfcfb9a6b07c5cc602
Michael Kreil <github@michael-kreil.de>
2021-01-07 13:11:36 +0100

    error handling in cronjob

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -8,15 +8,25 @@ cd "$(dirname "$0")"
 
 git pull
 
-node 1_download.js
-node 2_deduplicate.js
-node 3_parse.js
-node 4_generate_csv.js
+signalNoUpdate=0
+signalUpdate=42
+
+{
+	node 1_download.js &&
+	node 2_deduplicate.js &&
+	node 3_parse.js &&
+	node 4_generate_csv.js
+} || {
+	error=$?
+	signalNoUpdate=$error
+	signalUpdate=$error
+	echo "ERROR happend $error"
+}
 
 sleep 3
 
 git add ../data/
-git commit -m "automatic data update" || exit 0
+git commit -m "automatic data update" || exit $signalNoUpdate
 git push
 
-exit 42
+exit $signalUpdate
