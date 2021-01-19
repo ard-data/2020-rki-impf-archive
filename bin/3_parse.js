a51c790b9f89a06f64ce5ac8c6560acdf9a12ecf
Michael Kreil <github@michael-kreil.de>
2021-01-19 14:17:48 +0100

    fixing parser

--- bin/3_parse.js
+++ bin/3_parse.js
@@ -43,8 +43,6 @@ files.forEach(filename => {
 function parseExcel(filename) {
 	const letters = Object.fromEntries(',A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(',').map((c,i) => [c,i]));
 
-	let excel = {sheets: []}
-
 	// unzip excel file
 	let zip = new AdmZip(filename);
 
@@ -81,9 +79,11 @@ function parseExcel(filename) {
 		delete sheet.node;
 	})
 
-
-	console.dir(sheets, {depth:6});
-	process.exit();
+	return {
+		sheets,
+		parseAddress,
+		parseRange,
+	}
 
 	function extractCells(sheet) {
 		let cells = [];
@@ -96,15 +96,11 @@ function parseExcel(filename) {
 
 		// fix merged cells
 		select('/a:worksheet/a:mergeCells/a:mergeCell', sheet).forEach(node => {
-			let range = node.getAttribute('ref').split(':').map(parseAddress);
-			let colMin = Math.min(range[0].col, range[1].col);
-			let colMax = Math.max(range[0].col, range[1].col);
-			let rowMin = Math.min(range[0].row, range[1].row);
-			let rowMax = Math.max(range[0].row, range[1].row);
+			let range = parseRange(node.getAttribute('ref'));
 
-			let v = cells[rowMin][colMin];
-			for (let row = rowMin; row <= rowMax; row++) {
-				for (let col = colMin; col <= colMax; col++) {
+			let v = cells[range.rowMin][range.colMin];
+			for (let row = range.rowMin; row <= range.rowMax; row++) {
+				for (let col = range.colMin; col <= range.colMax; col++) {
 					cells[row][col] = v;
 				}
 			}
@@ -124,225 +120,195 @@ function parseExcel(filename) {
 
 			return {col, row, value};
 		}
+	}
 
-		function parseAddress(range) {
-			range = range.split(/([0-9]+)/);
-			return {
-				col: colToInt(range[0])-1,
-				row: parseInt(range[1], 10)-1,
-			}
+	function parseAddress(range) {
+		range = range.split(/([0-9]+)/);
+		return {
+			col: colToInt(range[0])-1,
+			row: parseInt(range[1], 10)-1,
+		}
 
-			function colToInt(col) {
-				return col.trim().split('').reduce((n, c) => n*26 +letters[c], 0);
-			}
+		function colToInt(col) {
+			return col.trim().split('').reduce((n, c) => n*26 +letters[c], 0);
+		}
+	}
+
+	function parseRange(range) {
+		range = range.split(':').map(parseAddress);
+		return {
+			colMin: Math.min(range[0].col, range[1].col),
+			colMax: Math.max(range[0].col, range[1].col),
+			rowMin: Math.min(range[0].row, range[1].row),
+			rowMax: Math.max(range[0].row, range[1].row),
 		}
 	}
 }
 
+function extractData(excel) {
+	let sheets = {};
+	excel.sheets.forEach(sheet => {
+		sheet.type = new Set();
+		if (sheet.name === 'Erläuterung') sheet.type.add('front');
+		if (sheet.name.startsWith('Gesamt_bis_einschl_')) sheet.type.add('hersteller');
+		if (sheet.name.startsWith('Indik_bis_einschl_')) sheet.type.add('indikation');
+		if (sheet.name === 'Impfungen_proTag') sheet.type.add('timeline');
+		if (sheet.type.size === 0) throw Error('Unbekanntes Tabellenblatt: "'+sheet.name+'"');
+		if (sheet.type.size > 1) throw Error('Tabellenblatt nicht eindeutig: "'+sheet.name+'"');
+		sheet.type = Array.from(sheet.type.values()).pop();
+		if (sheets[sheet.type]) throw Error('Tabellenblatt-Typ gibt es doppelt? "'+sheets[sheet.type].name+'" und "'+sheet.name+'"');
+		sheets[sheet.type] = sheet;
+	})
 
+	if (!sheets.front) throw Error('Frontblatt fehlt');
+	if (!sheets.indikation) throw Error('Indikationsblatt fehlt');
 
+	let pubDate = extractPubDate(sheets.front);
+	let date = extractDate(sheets.front);
 
-/*
-const letters = Object.fromEntries(',A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(',').map((c,i) => [c,i]));
-// Excel column headers
-const _excelColHeaders = [
-	{name:'impfungen_kumulativ',          text:'Impfungen kumulativ'},
-	{name:'differenz_zum_vortag',         text:'Differenz zum Vortag'},
-	{name:'impfungen_pro_1000_einwohner', text:'Impfungen pro 1.000 Einwohner', optional:true},
-	{name:'indikation_nach_alter',        text:'Indikation nach Alter'},
-	{name:'berufliche_indikation',        text:'Berufliche Indikation'},
-	{name:'medizinische_indikation',      text:'Medizinische Indikation'},
-	{name:'pflegeheimbewohnerin',         text:'Pflegeheim-bewohnerIn'},
-];
-// Excel row headers
-const _excelRowHeaders = [
-	{name:'BW', text:'Baden-Württemberg'},
-	{name:'BY', text:'Bayern'},
-	{name:'BE', text:'Berlin'},
-	{name:'BB', text:'Brandenburg'},
-	{name:'HB', text:'Bremen'},
-	{name:'HH', text:'Hamburg'},
-	{name:'HE', text:'Hessen'},
-	{name:'MV', text:'Mecklenburg-Vorpommern'},
-	{name:'NI', text:'Niedersachsen'},
-	{name:'NW', text:'Nordrhein-Westfalen'},
-	{name:'RP', text:'Rheinland-Pfalz'},
-	{name:'SL', text:'Saarland'},
-	{name:'SN', text:'Sachsen'},
-	{name:'ST', text:'Sachsen-Anhalt'},
-	{name:'SH', text:'Schleswig-Holstein'},
-	{name:'TH', text:'Thüringen'},
-	{name:'DE', text:'Gesamt'},
+	let data = {date, pubDate, states: {
+		BW:{},BY:{},BE:{},BB:{},HB:{},HH:{},HE:{},MV:{},NI:{},NW:{},RP:{},SL:{},SN:{},ST:{},SH:{},TH:{},DE:{}
+	}};
 
-	{text:'Anmerkung zu den Indikationen: Es können mehrere Indikationen je geimpfter Person vorliegen.', ignore:true},
-	{text:'einschl. Korrekturmeldung vom 27.12.20', ignore:true},
-	{text:'undefined', ignore:true},
-	{text:'In Sachsen-Anhalt wurde bereits am 26.12.2020 mit den Impfungen begonnen.', ignore:true},
-	{text:'In einigen Bundesländern werden nicht alle der in der Tabelle aufgeführten Indikationen einzeln ausgewiesen.', ignore:true},
-	{text:'in einigen Bundesländern werden nicht alle der in der Tabelle aufgeführten Indikationen einzeln ausgewiesen', ignore:true},
-	{text:'Anmerkung zu den Indikationen: es können mehrere Indikationen je geimpfter Person vorliegen', ignore:true},
-];
+	extractIndikation(data.states, sheets.indikation, pubDate);
+	extractHersteller(data.states, sheets.hersteller, pubDate);
 
+	data.germany = data.states.DE;
+	delete data.states.DE;
 
+	return data;
 
-// scan XLSX folder
-let todos = [];
-fs.readdirSync(dirSrc).forEach(filename => {
-	// ignore anything else than impfquotenmonitoring
-	if (!/^impfquotenmonitoring-202.*\.xlsx$/.test(filename)) return;
+	function extractPubDate(sheet) {
+		let rows = sheet.cells.map(r => r.join('\t'));
+		let dateString = [rows[2], rows[5]].join('\t');
+		let match;
 
-	// full name of source XLSX file and resulting JSON file
-	let fullnameSrc = resolve(dirSrc, filename);
-	let fullnameDst = resolve(dirDst, filename.replace(/\.xlsx$/i, '.json'));
-
-	// ignore, when JSON file already exists
-	if (fs.existsSync(fullnameDst)) return;
-
-	console.log('parse '+filename);
-
-	// unzip excel file
-	let zip = new AdmZip(fullnameSrc);
-
-	// find the 4 XML files we need
-	let workbook, sheetFront, sheetData, strings;
-	zip.getEntries().forEach(e => {
-		if (e.entryName.endsWith('xl/workbook.xml')) return workbook = p(e); // get workbook
-		if (e.entryName.endsWith('xl/worksheets/sheet1.xml')) return sheetFront = p(e); // get front sheet
-		if (e.entryName.endsWith('xl/worksheets/sheet2.xml')) return sheetData = p(e); // get data sheet
-		if (e.entryName.endsWith('xl/sharedStrings.xml')) return strings = p(e); // get shared strings
-
-		function p(e) {
-			return new DOMParser().parseFromString(e.getData().toString('utf8'));
+		if (match = dateString.match(/^\t?Datenstand: (\d\d)\.(\d\d)\.(\d\d\d\d), (\d\d:\d\d) Uhr\t/)) {
+			return match[3]+'-'+match[2]+'-'+match[1]+' '+match[4];
 		}
-	})
-	
-	// extract shared strings
-	strings = select('//a:si', strings).map(string => select('.//a:t[not(ancestor::a:rPh)]', string).map(node => node.textContent).join(''));
 
-	// extract front sheet name
-	let sheetDataName = false;
-	select('/a:workbook/a:sheets/a:sheet', workbook).forEach(node => {
-		if (node.getAttribute('r:id') === 'rId2') sheetDataName = node.getAttribute('name');
-	})
-
-	// extract front sheet cell content
-	let sheetFrontCells = extractCells(sheetFront);
-	let date = parseDate(filename, sheetDataName, sheetFrontCells);
-
-	// extract data sheet cell content
-	let sheetDataCells = extractCells(sheetData);
-
-	let colOffset = 0;
-	let rowOffset = 0;
-	if (date >= '2021-01-07') colOffset = 1;
-	
-	// check headers
-	let excelColHeaders = prepareHeaderDefinition(_excelColHeaders, colOffset, sheetDataCells[rowOffset]);
-	let excelRowHeaders = prepareHeaderDefinition(_excelRowHeaders, rowOffset, sheetDataCells.map(r => r[colOffset]));
-
-	// read data from Excel file to data structure
-	let data = {date, states:{}};
-	excelRowHeaders.forEach(r => {
-		let obj = {
-			code:r.name,
-			title:r.text,
+		if (match = dateString.match(/^\tDatenstand: 28\.12\.2020, 08:00 Uhr\t(44\d\d\d)\t(\d\d:\d\d) Uhr/)) {
+			let d = (parseFloat(match[1])-25568.5)*86400000;
+			d = (new Date(d)).toISOString();
+			d = d.substr(0,10)+' '+match[2];
+			return d;
 		}
-		excelColHeaders.forEach(c => obj[c.name] = sheetDataCells[r.index][c.index])
-		if (r.name === 'DE') return data.germany = obj;
-		data.states[r.name] = obj;
-	})
 
-	// save data structure as JSON
-	fs.writeFileSync(fullnameDst, JSON.stringify(data, null, '\t'));
+		if (dateString.startsWith('Datenstand: 28.12.2020, 08:00 Uhr\t44200\t12:00 Uhr')) return '2021-01-04 12:00';
 
+		if (match = dateString.match(/^Datenstand: (\d\d)\.(\d\d)\.(\d\d\d\d), (\d\d:\d\d) Uhr\tNaN\tNaN\tNaN\t/)) {
+			return match[3]+'-'+match[2]+'-'+match[1]+' '+match[4];
+		}
 
+		console.log(dateString);
+		throw Error('Can not parse pub date');
+	}
 
-	function extractCells(sheet) {
-		let cells = [];
-		select('/a:worksheet/a:sheetData/a:row/a:c', sheet).forEach(node => {
-			let cell = parseCell(node);
+	function extractDate(sheet) {
+		let rows = sheet.cells.map(r => r.join('\t'));
+		let dateString = rows[4];
+		let match;
 
-			if (!cells[cell.row]) cells[cell.row] = [];
-			cells[cell.row][cell.col] = cell.value;
-		});
-		return cells;
+		if (match = dateString.match(/^Durchgeführte Impfungen bundesweit und nach Bundesland sowie nach STIKO-Indikation bis einschließlich (\d\d)\.(\d\d)\.(\d\d) \(Impfungen_bis_einschl_/)) {
+			return '20'+match[3]+'-'+match[2]+'-'+match[1];
+		}
 
-		function parseCell(node) {
-			let range = node.getAttribute('r').split(/([0-9]+)/);
-			let col = colToInt(range[0])-1;
-			let row = parseInt(range[1])-1;
-			let value = (select('a:v', node, 1) || {textContent: ''}).textContent;
-			let type = node.getAttribute('t') || '';
+		console.log(dateString);
+		throw Error('Can not parse date');
+	}
 
-			switch (type) {
-				case 's': value = strings[parseInt(value, 10)]; break;
-				case '': value = parseFloat(value); break;
-				default: throw Error('unknown cell type '+type);
-			}
-
-			return {col, row, value};
-
-			function colToInt(col) {
-				return col.trim().split('').reduce((n, c) => n*26 +letters[c], 0);
-			}
+	function extractIndikation(data, sheet, pubDate) {
+		try {
+			return extractDataSheet(data, sheet.cells, excel.parseRange('C3:J19'));
+		} catch (e) {
+			console.log('in sheet "'+sheet.name+'":');
+			throw e;
 		}
 	}
 
-	function prepareHeaderDefinition(_def, offset, data) {
-		let def = _def.map(e => {
-			let entry = Object.assign({}, e);
-			entry.use = false;
-			return entry;
-		});
+	function extractHersteller(data, sheet, pubDate) {
+		try {
+			return extractDataSheet(data, sheet.cells, excel.parseRange('C4:I20'));
+		} catch (e) {
+			console.log('in sheet "'+sheet.name+'":');
+			throw e;
+		}
+	}
+	function extractDataSheet(data, cells, range) {
+		for (let row = range.rowMin; row <= range.rowMax; row++) {
+			for (let col = range.colMin; col <= range.colMax; col++) {
+				let rowId = parseRowHeader(mergeRowCells(cells, row, 0, range.colMin-1));
+				let colId = parseColHeader(mergeColCells(cells, col, 0, range.rowMin-1));
 
-		for (let i = 1+offset; i < data.length; i++) {
-			let value = (''+data[i]).replace(/\*+/g,'').trim();
-			let entry = def.find(e => e.text === value);
+				data[rowId][colId] = cells[row][col];
+			}
+		}
+		return data;
+	}
+
+	function mergeRowCells(cells, row, colMin, colMax) {
+		return cells[row].slice(colMin, colMax+1).join('\t');
+	}
+
+	function mergeColCells(cells, col, rowMin, rowMax) {
+		return cells.slice(rowMin, rowMax+1).map(r => r[col]).join('\t');
+	}
+
+	function parseRowHeader(text) {
+		switch (text) {
+			case '08\tBaden-Württemberg':      return 'BW';
+			case '09\tBayern':                 return 'BY';
+			case '11\tBerlin':                 return 'BE';
+			case '12\tBrandenburg':            return 'BB';
+			case '04\tBremen':                 return 'HB';
+			case '02\tHamburg':                return 'HH';
+			case '06\tHessen':                 return 'HE';
+			case '13\tMecklenburg-Vorpommern': return 'MV';
+			case '03\tNiedersachsen':          return 'NI';
+			case '05\tNordrhein-Westfalen':    return 'NW';
+			case '07\tRheinland-Pfalz':        return 'RP';
+			case '10\tSaarland':               return 'SL';
+			case '14\tSachsen':                return 'SN';
+			case '15\tSachsen-Anhalt':         return 'ST';
+			case '01\tSchleswig-Holstein':     return 'SH';
+			case '16\tThüringen':              return 'TH';
+			case '\tGesamt':                   return 'DE';
+		}
+
+		throw Error('unknown Row Header '+JSON.stringify(text))
+	}
+	function parseColHeader(text) {
+		switch (text) {
+			case 'Gesamtzahl bisher verabreichter Impfstoffdosen\tGesamtzahl bisher verabreichter Impfstoffdosen\tGesamtzahl bisher verabreichter Impfstoffdosen': return 'impfungen_kumulativ';
+
+			case 'Erstimpfung\tImpfungen kumulativ\tGesamt': return 'impfungen_kumulativ_erstimpfung';
+			case 'Zweitimpfung\tImpfungen kumulativ\tImpfungen kumulativ': return 'impfungen_kumulativ_zweitimpfung';
+
+			case 'Erstimpfung\tImpfungen kumulativ\tBioNTech': return 'impfungen_kumulativ_by_biontech_erstimpfung';
+			case 'Zweitimpfung\tImpfungen kumulativ\tBioNTech': return 'impfungen_kumulativ_by_biontech_zweitimpfung';
+
+			case 'Erstimpfung\tImpfungen kumulativ\tModerna': return 'impfungen_kumulativ_by_moderna_erstimpfung';
+			case 'Zweitimpfung\tImpfungen kumulativ\tModerna': return 'impfungen_kumulativ_by_moderna_zweitimpfung';
+
+			case 'Erstimpfung\tDifferenz zum Vortag\tDifferenz zum Vortag': return 'differenz_zum_vortag_erstimpfung';
+			case 'Zweitimpfung\tDifferenz zum Vortag\tDifferenz zum Vortag': return 'differenz_zum_vortag_zweitimpfung';
+
+			case 'Erstimpfung\tImpf-quote, %\tImpf-quote, %': return 'impfungen_prozent_erstimpfung';
+			case 'Zweitimpfung\tImpf-quote, %\tImpf-quote, %': return 'impfungen_prozent_zweitimpfung';
+
+			case 'Erstimpfung\tIndikation nach Alter*': return 'indikation_nach_alter_erstimpfung';
+			case 'Zweitimpfung\tIndikation nach Alter*': return 'indikation_nach_alter_zweitimpfung';
 			
-			if (!entry) throw Error('"'+value+'" ('+JSON.stringify(data)+') not found in '+JSON.stringify(def));
-			entry.index = i;
-
-			if (!entry.ignore && entry.use) throw Error('"'+value+'" ('+JSON.stringify(data)+') already in use '+JSON.stringify(def));
-			entry.use = true;
+			case 'Erstimpfung\tBerufliche Indikation*': return 'berufliche_indikation_erstimpfung';
+			case 'Zweitimpfung\tBerufliche Indikation*': return 'berufliche_indikation_zweitimpfung';
+			
+			case 'Erstimpfung\tMedizinische Indikation*': return 'medizinische_indikation_erstimpfung';
+			case 'Zweitimpfung\tMedizinische Indikation*': return 'medizinische_indikation_zweitimpfung';
+			
+			case 'Erstimpfung\tPflegeheim-bewohnerIn*': return 'pflegeheimbewohnerin_erstimpfung';
+			case 'Zweitimpfung\tPflegeheim-bewohnerIn*': return 'pflegeheimbewohnerin_zweitimpfung';
 		}
 
-		def = def.filter(entry => {
-			if (entry.ignore) return false;
-			if (entry.use) return true;
-			if (entry.optional) return false;
-			throw Error(JSON.stringify(entry));
-		})
-
-		return def;
+		throw Error('unknown Col Header '+JSON.stringify(text))
 	}
-})
-
-
-function parseDate(filename, sheetName, cells) {
-	// parses date and returns it as string like "2021-01-01 12:30"
-	let match;
-	let rows = cells.map(r => r.join('\t'));
-	let dateStrings = [rows[2], rows[5], sheetName].join('\t');
-
-	if (match = dateStrings.match(/^\tDatenstand: (\d\d)\.(\d\d)\.(\d\d\d\d), (\d\d:\d\d) Uhr\t/)) {
-		return match[3]+'-'+match[2]+'-'+match[1]+' '+match[4];
-	}
-
-	if (match = dateStrings.match(/^\tDatenstand: 28\.12\.2020, 08:00 Uhr\t(44\d\d\d)\t(\d\d:\d\d) Uhr/)) {
-		let d = (parseFloat(match[1])-25568.5)*86400000;
-		d = (new Date(d)).toISOString();
-		d = d.substr(0,10)+' '+match[2];
-		return d;
-	}
-
-	if (dateStrings.startsWith('Datenstand: 28.12.2020, 08:00 Uhr\t44200\t12:00 Uhr')) return '2021-01-04 12:00';
-
-	if (match = dateStrings.match(/^Datenstand: (\d\d)\.(\d\d)\.(\d\d\d\d), (\d\d:\d\d) Uhr\tNaN\tNaN\tNaN\t/)) {
-		return match[3]+'-'+match[2]+'-'+match[1]+' '+match[4];
-	}
-
-	console.log(filename, dateStrings);
-
-	throw Error('Can not parse date');
 }
-*/
