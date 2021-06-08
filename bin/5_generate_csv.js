82402f15fe22d0345b4ee95878304e6da0d6efd2
Michael Kreil <github@michael-kreil.de>
2021-06-08 18:08:41 +0200

    use updated data definition

--- bin/5_generate_csv.js
+++ bin/5_generate_csv.js
@@ -4,7 +4,7 @@
 
 const fs = require('fs');
 const {resolve} = require('path');
-const dataDefinition = require('../config/data_definition.js')('2021-04-08');
+const dataDefinition = require('../config/data_definition.js')('2021-06-07');
 
 
 
